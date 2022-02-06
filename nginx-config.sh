8ae09df0048c8ad05b25db0c9535abfec9a92b4b
Josenilto L da Silva <josenilto@outlook.com>
2022-02-06 05:46:06 -0300

    Update nginx-config.sh

--- nginx-config.sh
+++ nginx-config.sh
@@ -1,11 +1,46 @@
-sudo yum install epel-release -y &&
+#!/bin/bash
 
-sudo yum install nginx - y &&
+echo ""
+echo "Atualizando o sistema..."
+echo ""
 
-sudo systemctl start nginx.service &&
+sudo um -y update && sudo yum -y upgrade &&
+
+echo ""
+echo "Atualizando o sistema..."
+echo ""
+
+sudo yum -y install epel-release &&
+
+echo ""
+echo "Instalar o nginx..."
+echo ""
+
+sudo yum -y install nginx &&
+
+echo ""
+echo "Inicializar com  sistema..."
+echo ""
+
+sudo systemctl start nginx.service  -y &&
+
+echo ""
+echo "Liberando a porta http..."
+echo ""
 
 sudo firewall-cmd --permanent --zone=public --add-service=http &&
 
+echo ""
+echo "Liberando a porta https..."
+echo ""
+
 sudo firewall-cmd --permanent --zone=public --add-service=https &&
 
+echo ""
+echo "Atualizando o firewall..."
+echo ""
+
 sudo firewall-cmd --reload
+
+echo ""
+

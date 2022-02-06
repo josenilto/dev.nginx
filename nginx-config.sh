61cce85510e545e5e0f583cf816a8fd2a4cf2efe
Josenilto L da Silva <josenilto@outlook.com>
2022-02-06 05:47:13 -0300

    atualização do projeto sh

--- nginx-config.sh
+++ nginx-config.sh
@@ -1,5 +1,5 @@
-sudo yum install epel-release &&
-sudo yum install nginx &&
+sudo yum install epel-release -y &&
+sudo yum install nginx -y &&
 sudo systemctl start nginx.service &&
 sudo firewall-cmd --permanent --zone=public --add-service=http &&
 sudo firewall-cmd --permanent --zone=public --add-service=https &&
